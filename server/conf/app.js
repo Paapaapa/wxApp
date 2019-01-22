8acd23282258c3bdb4365a4f93aa96ade621e0f4
Paapaapa <304752464@qq.com>
2019-01-22 12:49:14 +0800

    first

---
+++ server/conf/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+    appConfig: {
+        appid: 'wxed0cc4ccfc60aa9d',
+        secret: '0f7849a44eb4a88930819b2e22009648'
+    },
+    userConfig: {
+        credit: 30          // 用户初始积分余额
+    }
+}
